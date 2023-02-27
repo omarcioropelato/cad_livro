8db3028782cacd3065e173123bb8ac23953bcf72
omarcioropelato <marcio.ropelato@gmail.com>
2023-02-27 20:47:20 -0300

    alteracoes no banner

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -2,17 +2,36 @@ import './Banner.css'
 
 function Banner() {
     return (
-        <div className='container'>
-            <div className='informacoes-textuais'>
-                <h1>Cadastro de Livros</h1>
-                <p>Sistema criado para o cadastro de livros da biblioteca do IPC.</p>
-            </div>
+        <section className='header'>
+            <div className='container'>
 
-            <div className='imagens'>
-                <img src='./imagens/livros.jpg' alt='Banner da Página' />
-            </div>
+                <div className='titulo'>
 
-        </div>
+                    <div className='logo-ipc'>
+                        <img src='./imagens/logo_IPC.png' alt='Logo do IPC' />
+                    </div>
+
+                    <div className='links'>
+                        <ul>
+                            <li>Item 1</li>
+                            <li>Item 2</li>
+                            <li>Item 3</li>
+                        </ul>
+                    </div>
+
+                </div>
+
+                <div className='informacoes-textuais'>
+                    <h1>Cadastro de Livros</h1>
+                    <p>Sistema criado para o cadastro de livros da biblioteca do IPC.</p>
+                </div>
+
+                <div className='imagens'>
+                    <img src='./imagens/livros.jpg' alt='Banner da Página' />
+                </div>
+
+            </div>
+        </section>
     )
 }
 
